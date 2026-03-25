52477a344af9d7e20f0992869e2f780f82d00390
nnupur20 <nupuragarwal20@gmail.com>
2026-03-25 16:06:38 +0530

    Initialize React application with NavanaaEstate component

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import NavanaaEstate from '../NavanaaEstate';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <NavanaaEstate />
+        </React.StrictMode>
+        );
